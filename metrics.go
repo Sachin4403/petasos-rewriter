670ad98e18940f9a8bb367ba7a8ec0c7e510636a
chaitanya.singla <chaitanya.singla@telekom-digital.com>
2023-06-23 18:26:50 +0530

    Add content encoding type as identity in metrics middleware

--- metrics.go
+++ metrics.go
@@ -44,6 +44,7 @@ func getMiddleware() echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
 			startTime := time.Now()
+			c.Response().Header().Set("Content-Encoding", "identity")
 			err := next(c)
 			elapsed := time.Since(startTime).Seconds()
 			status := strconv.Itoa(c.Response().Status)
